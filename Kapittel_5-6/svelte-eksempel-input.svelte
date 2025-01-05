a81626305b7bcc4c7a0c278ca6ea1937ec12f0c3
johanhake <johan.hake@gmail.com>
2025-01-05 15:19:31 +0100

    La til de første programmeringseksemplene

---
+++ Kapittel_5-6/svelte-eksempel-input.svelte
@@ -0,0 +1,15 @@
+<script>
+	let liker = 'koding';
+</script>
+
+<h1>Eksempel på dynamisk Nettside med JavaScript</h1>
+<p>
+	Jeg liker: {liker}
+</p>
+<input bind:value={liker}>
+
+<style>
+	*{
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+	}
+</style>
